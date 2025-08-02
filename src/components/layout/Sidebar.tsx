3107dc05559d9a767b7157b851b5cc9dff6ea08c
bit2swaz <bit2swaz@gmail.com>
2025-08-03 00:00:51 +0530

    remove resolution control entirely

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -4,15 +4,11 @@ import React from "react";
 import TextControl from "~/components/editor/controls/TextControl";
 import StyleControl from "~/components/editor/controls/StyleControl";
 import BackgroundControl from "~/components/editor/controls/BackgroundControl";
-import ResolutionControl from "~/components/editor/controls/ResolutionControl"; // Import the new component
 
 const Sidebar = () => {
   return (
     <aside className="fixed top-16 left-0 z-10 h-[calc(100vh-4rem)] w-80 overflow-y-auto border-r border-gray-700 bg-gray-800 p-6">
       <div className="space-y-6">
-        {/* Add the new resolution control at the top */}
-        <ResolutionControl />
-
         <div className="h-px bg-gray-700"></div>
         <TextControl />
         <div className="h-px bg-gray-700"></div>
