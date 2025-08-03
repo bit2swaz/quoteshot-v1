934a30f91168ec0455d8be66d27fe1cb9e6644d3
bit2swaz <bit2swaz@gmail.com>
2025-08-04 00:38:22 +0530

    attempt to fix deploy error

--- src/components/editor/ClientOnlyEditor.tsx
+++ src/components/editor/ClientOnlyEditor.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 "use client";
