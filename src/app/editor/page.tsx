95d62109ac1ffad0b9beddb61264c3505ae95cb7
bit2swaz <bit2swaz@gmail.com>
2025-08-06 00:02:18 +0530

    fix mobile blocker component on /editor page

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -4,7 +4,6 @@ import dynamic from "next/dynamic";
 import React from "react";
 import { useCanvasStore } from "~/store/canvasStore";
 import WelcomeModal from "~/components/editor/WelcomeModal";
-import MobileBlocker from "~/components/layout/MobileBlocker";
 
 const ClientOnlyEditor = dynamic(
   () => import("~/components/editor/ClientOnlyEditor"),
@@ -20,17 +19,9 @@ export default function EditorPage() {
   const { isWelcomeModalOpen } = useCanvasStore();
 
   return (
-    <>
-      {/* Desktop Content: This will be hidden on screens smaller than 'lg' */}
-      <div className="hidden h-full w-full lg:block">
-        <ClientOnlyEditor />
-        {isWelcomeModalOpen && <WelcomeModal />}
-      </div>
-
-      {/* Mobile Blocker: This will only be visible on screens smaller than 'lg' */}
-      <div className="lg:hidden">
-        <MobileBlocker />
-      </div>
-    </>
+    <div className="h-full w-full">
+      <ClientOnlyEditor />
+      {isWelcomeModalOpen && <WelcomeModal />}
+    </div>
   );
 }
